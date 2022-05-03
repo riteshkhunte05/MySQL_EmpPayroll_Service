3fe3f9be4ca32670141383b4bb1dcb3475f9b16c
riteshkhunte05 <riteshkhunte05@gmail.com>
2022-05-04 01:16:05 +0530

    Added: Create Employee Payroll Data in Payroll Service DB

---
+++ UC3.sql
@@ -0,0 +1,2 @@
+use employee_payroll;
+INSERT INTO employee_payroll( name, salary, start) VALUES('virat', 100000.00, '2018-01-03' ),('Rohit', 200000.00, '2019-11-13'),('Dhoni', 300000.00, '2020-05-21' );
