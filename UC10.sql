2dd8abb925fca65d65906b1b7ddf4432db8103b6
riteshkhunte05 <riteshkhunte05@gmail.com>
2022-05-06 19:37:21 +0530

    Added: Make Terissa as part of Sales & Marketing dept

---
+++ UC10.sql
@@ -0,0 +1,2 @@
+insert into Employee_payroll (id,name,phone_number,address,department,gender,basic_pay,deductions,taxable_pay,tax,net_pay,start) values(5,'Terissa','9888888888','Hydrabad','Sales','F',4800000,100000,40000,5400,4000000,'2021-05-06');
+insert into Employee_payroll (id,name,phone_number,address,department,gender,basic_pay,deductions,taxable_pay,tax,net_pay,start) values(6,'Terissa','9888888888','Banglore','Marketing','F',4800000,100000,40000,5400,4000000,'2021-05-06');
