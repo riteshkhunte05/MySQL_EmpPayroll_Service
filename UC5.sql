22f57ed0939e9a06a04c133471f5aedcfbb1ddff
riteshkhunte05 <riteshkhunte05@gmail.com>
2022-05-06 18:54:34 +0530

    Added: Retrieve Salary data of a particular Employee

---
+++ UC5.sql
@@ -0,0 +1,4 @@
+use employee_payroll;
+select * from employee_payroll;
+select * from Employee_payroll where name = 'virat';
+select * from Employee_payroll where start between '2018-01-03' and date(now());
